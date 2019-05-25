76b85f4b08d1655bbada20214ba7d86155c6d49f
joaopedroa.6 <joaopedroa.6@hotmail.com>
2019-05-25 20:07:34 -0300

    Primero Commit segurancao

---
+++ saude/src/main/java/com/saude/saude/model/RoleName.java
@@ -0,0 +1,7 @@
+package com.saude.saude.model;
+
+public enum RoleName {
+	ROLE_USER,
+    ROLE_PM,
+    ROLE_ADMIN
+}
---
+++ saude/src/main/java/com/saude/saude/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.saude.saude.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.saude.saude.model.Role;
+import com.saude.saude.model.RoleName;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
